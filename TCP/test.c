2961d5f9d901f558fed9835493fce7d9b594f8a2
Song812ads <song.tran91105@hcmut.edu.vn>
2023-08-04 15:58:12 +0700

    dada

--- TCP/test.c
+++ TCP/test.c
@@ -9,8 +9,33 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
+
 #define BUFFLEN 5000
 typedef enum {FIRST, AFTER} file_mode;
+
+
+ssize_t  readn(int fd, void *vptr, size_t n)
+{    size_t  nleft;
+    ssize_t nread;
+    char   *ptr;
+
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
+        if ( (nread = read(fd, ptr, nleft)) < 0) {
+            if (errno == EINTR)
+                nread = 0;      /* and call read() again */
+            else
+                return (-1);
+        } else if (nread == 0)
+            break;              /* EOF */
+
+        nleft -= nread;
+       ptr += nread;
+    }
+     return (n - nleft);         /* return >= 0 */
+}
+
 
 int checkfile(char* buffer){
     if (access(buffer, F_OK) == -1){
@@ -27,23 +52,45 @@ int checkfile(char* buffer){
     }
 }
 
-long file_transfer(char* buffer, int t){
-    int fp = open(buffer, O_RDONLY);
-    if (fp == -1){
-        perror("Error reading file\n");
-        exit(1);
-    }
-    long offset = 0;
-    while (offset < BUFFLEN){
-        ssize_t readnow = pread(fp, buffer+offset, 1, t*BUFFLEN + offset);
-        if (readnow == 0){
-            break;
+// long file_transfer(char* buffer, int t){
+//     int fp = open(buffer, O_RDONLY);
+//     if (fp == -1){
+//         perror("Error reading file\n");
+//         exit(1);
+//     }
+//     long offset = 0;
+//     while (offset < BUFFLEN){
+//         ssize_t readnow = readn(fp, buffer+offset, 1);
+//         if (readnow == 0){
+//             break;
+//         }
+//         else offset = offset+readnow;
+//     }
+//     close(fp);
+//     printf("File read complete \n");
+//     return offset;
+// }
+
+ ssize_t                         /* Write "n" bytes to a descriptor. */
+writen(int fd, const void *vptr, size_t n)
+ {
+    size_t nleft;
+     ssize_t nwritten;
+    const char *ptr;
+
+  ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
+        if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+            if (nwritten < 0 && errno == EINTR)
+               nwritten = 0;   /* and call write() again */
+           return (-1);    /* error */
         }
-        else offset = offset+readnow;
+
+      nleft -= nwritten;
+        ptr += nwritten;
     }
-    close(fp);
-    printf("File read complete \n");
-    return offset;
+  return (n);
 }
 
 void file_transfer1(char* file, char* buffer, long size, int t, file_mode mode){
@@ -54,7 +101,7 @@ void file_transfer1(char* file, char* buffer, long size, int t, file_mode mode){
     }
     off_t offset = 0;
     for (int i=0; i < size; i++){
-        ssize_t readnow = pwrite(fp, buffer + offset, 1,t*BUFFLEN + offset);
+        ssize_t readnow = writen(fp, buffer + offset, 1);
         if (readnow < 0){
             printf("Read unsuccessful \n");
             free(buffer);
@@ -81,7 +128,7 @@ void file_transfer1(char* file, char* buffer, long size, int t, file_mode mode){
 //     // memset(buffer,'\0',BUFFLEN);
 //     long offset = 0;
 //     while  (offset<BUFFLEN){
-//         size_t readnow = fread(buffer+offset, 1,1, fp);
+//         size_t readnow = readn(buffer+offset, fp,1);
 //         if (readnow == 0) break;
 //         else offset ++ ;
 //     }
@@ -132,11 +179,13 @@ int main()
     strcpy(path_buffer,path);
     strcpy(path_buffer+len,buffer);
     memset(buffer,'\0',BUFFLEN);
-    strcpy(buffer,path_buffer);
-
-    long size = file_transfer(buffer, 0);
+    // strcpy(buffer,path_buffer);
+    int open1 = open(path_buffer, O_RDONLY);
+    long size = readn(open1,buffer, BUFFLEN);
     file_mode mode = FIRST;
-    file_transfer1("anh.jpeg",buffer, size,0,mode);
+    printf("%s",buffer);
+    int open2 = open("anh.jpeg", O_RDWR | O_APPEND | O_CREAT | O_SYNC, 0644);
+    size = writen(open2,buffer,BUFFLEN);
     free(path_buffer);
     free(buffer);
     char msg[123]="12354";
